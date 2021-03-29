93a10593e210a30aa4c1f4a5357abfe863d6f99a
DevCoderGod <devcodergod@gmail.com>
2021-03-30 01:52:55 +0300

    v 1.0

---
+++ #src/js/script.js
@@ -0,0 +1,172 @@
+let table
+let cardsData
+
+document.addEventListener("DOMContentLoaded",() =>{
+	table=document.querySelector('.table')
+	cardsData=JSON.parse(getBackendData())
+	let count=0
+	let cards=document.querySelector('.table').querySelectorAll(".card")
+	let cardProgressText=document.querySelector(".card-progress__subtitle")
+	let cardProgressbar=document.querySelector(".card-progress__bar--fill")
+	putCards(cards,cardsData)
+
+	cards.forEach(card =>card.addEventListener('click',fliper))
+	function fliper() {
+		this.classList.add('card--flip')
+		count++
+		this.removeEventListener('click',fliper)
+		if(count==1) cardProgressMove('Pick Card 1',"0.1s linear","7.9%")
+		if(count==2) cardProgressMove('Pick Card 2',"0.6s linear","54%")
+		if(count==3){
+			cardProgressMove('Done',"0.4s linear","100%")
+			setTimeout(go(),500)
+		}
+		function cardProgressMove(text,anim,val) {
+			cardProgressText.textContent=text
+			cardProgressbar.style.transition=anim
+			cardProgressbar.style.width=val
+		}
+	}
+
+	document.querySelector('.form__input--submit').addEventListener('click',formSubmit)
+	document.querySelector('#google').addEventListener('click',authGoogle)
+	document.querySelector('#facebook').addEventListener('click',authFacebook)
+
+})
+
+
+
+function go() {
+	let progress=document.querySelector('.progress')
+	let start = new Promise(res=>setTimeout(() => res(), 2000))
+	start
+	.then(()=>{
+		table.innerHTML=''
+		progress.classList.remove('hidden')
+		return progress
+	})
+	.then((progress)=>{
+		setTimeout(() => {
+			progress.querySelector('.progress__fill').classList.add('progress__fill--full')
+		}, 100)
+		return progress
+	})
+	.then((progress)=>{
+		let fill=progress.querySelector('.progress__fill')
+		let text=progress.querySelector('.progress__text')
+		setTimeout(() => {
+			text.innerHTML='<p>Your Tarot reading is ready!</p><p>Click below to get it now:</p>'
+			fill.innerHTML='<p>Get my reading</p>'
+			return progress
+		}, 1500)
+		return fill
+	})
+	.then((fill)=>{
+		fill.addEventListener('click',showForm)
+		fill.style='cursor:pointer;'
+	})
+}
+
+function showForm() {
+	document.querySelector('#form').classList.remove('hidden')
+	document.querySelector('.card-progress').classList.add('hidden')
+	document.querySelector('.progress').classList.add('hidden')
+	document.querySelector('.table').classList.add('hidden')
+	document.querySelector('.form__input--submit').addEventListener('click',formSubmit)
+	document.querySelector('#google').addEventListener('click',authGoogle)
+	document.querySelector('#facebook').addEventListener('click',authFacebook)
+}
+
+async function formSubmit(e) {
+	e.preventDefault();
+	let form=document.querySelector('#form')
+	if(formValidate()) showResult(await sendData())
+
+	async function sendData(){
+		let formData= new FormData(form)
+		formData.append('ids', cardsData.map(i=>i.id))
+		let res=await fetch('/',{method: 'POST', body: formData})
+		return res
+	}
+
+	function formValidate() {
+		let err=false;
+		inputs=form.querySelectorAll('input')
+		inputs.forEach(el => inputValidate(el));
+		if(!err) return true
+
+		function inputValidate(input) {
+			if (input.classList.contains('mail')) {
+				if (!/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(input.value)){
+					err=true
+					input.classList.add('err')
+				}
+			}
+		}
+	}
+
+}
+
+function showResult(respons){
+	if(respons.ok){
+		let cards=JSON.parse(respons)
+	}
+	else{
+		document.querySelector('#form').classList.add('hidden')
+		document.querySelector('.advertisement2').classList.add('hidden')
+		let cards=document.querySelector('.prediction').querySelectorAll(".card")
+		putCards(cards,cardsData)
+		putText(document.querySelector('.prediction__text'),cardsData)
+		document.querySelector('.prediction').classList.remove('hidden')
+		document.querySelector('.advertisement3').classList.remove('hidden')
+	}
+}
+
+function authGoogle(e) {
+	e.preventDefault();
+}
+function authFacebook(e) {
+	e.preventDefault();
+}
+
+function randomId(min, max) {
+	let rand = min + Math.random() * (max + 1 - min);
+	return Math.floor(rand);
+}
+
+ function putCards(cards,data) {
+	i=0
+	cards.forEach(card =>{
+		card.querySelector('.card__side--front').innerHTML=
+		`<img src="img/${data[i].image}" alt="${data[i].imageAlt}">`
+		i++
+	})
+}
+
+function putText(text,data) {
+	text.innerHTML=`<p>${data[0].prediction}</p><p>${data[1].prediction}</p><p>${data[2].prediction}</p>`
+}
+
+ function getBackendData() { //return JSON [{},{},{}]
+	//let table=[0,1,4,7,8,36,69]
+	let idsAll=[0,1,2,3,4,5,6]
+	let idsCards=getCardsId()
+	let desk=JSON.parse(document.querySelector('#service').innerHTML)
+	return JSON.stringify(getCards())
+
+	function getCards() {
+		let cards=[]
+		for (let i = 0; i < 3; i++)cards[i]=desk[idsCards[i]]
+		return cards
+	}
+
+	function getCardsId() {
+		let ids=[]
+		for (let i = 6; i > 3; i--) {
+			let id=randomId(0,i)
+			ids.push(idsAll[id])
+			idsAll.splice(id,1)
+		}
+		return ids
+	}
+}
---
+++ gulpfile.js
@@ -0,0 +1,230 @@
+//--------------------------------------------------
+// function defaultTask(cb) {
+// 	// place code for your default task here
+// 	cb();
+//  }
+ 
+//  exports.default = defaultTask
+//--------------------------------------------------
+//ПЕРЕМЕННЫЕ ПАПОК
+//--------------------------------------------------
+//let project_folder = "dist"
+let project_folder = require("path").basename(__dirname)
+let source_folder = "#src"
+let fs = require("fs")
+let indexFile = "index_dev.html"
+//let indexFile = "index.html"
+
+//--------------------------------------------------
+//ПУТИ
+//--------------------------------------------------
+let path ={
+	build:{
+		html: project_folder+"/",
+		css: project_folder+"/css/",
+		js: project_folder+"/js/",
+		img: project_folder+"/img/",
+		fonts: project_folder+"/fonts/",
+	},
+	src:{
+		html: [source_folder+"/*.html", "!" + source_folder +"/_*.html"],
+		css: source_folder+"/scss/style.scss",
+		js: source_folder+"/js/script.js",
+		img: source_folder+"/img/**/*.{jpg,JPG,png,svg,gif,ico,webp}",
+		fonts: source_folder+"/fonts/*.{ttf,woff,woff2}",
+	},
+	watch:{
+		html: source_folder+"/**/*.html",
+		css: source_folder+"/scss/**/*.scss",
+		js: source_folder+"/js/**/*.js",
+		img: source_folder+"/img/**/*.{jpg,png,svg,gif,ico,webp}"
+	},
+	clean: "./"+project_folder+"/"
+}
+//--------------------------------------------------
+//ПЕРЕМЕННЫЕ
+//--------------------------------------------------
+let {src,dest}=require("gulp")
+let gulp=require('gulp')
+
+let browsersync=require("browser-sync").create();
+let fileinclude=require("gulp-file-include")
+let del=require("del")
+let scss=require("gulp-sass")
+let autoprefixer=require("gulp-autoprefixer")
+let gcmq = require("gulp-group-css-media-queries")
+let clean_css = require("gulp-clean-css");
+let rename = require("gulp-rename");
+let uglify = require('gulp-uglify-es').default;
+let imagemin = require('gulp-imagemin');
+let ttf2woff = require('gulp-ttf2woff');
+let ttf2woff2 = require('gulp-ttf2woff2');
+let sourcemaps = require('gulp-sourcemaps');
+//--------------------------------------------------
+//browser-sync
+//--------------------------------------------------
+function BrowserSync(params) {
+	browsersync.init({
+		server:{
+			baseDir:"./"+project_folder+"/"
+		},
+		index: indexFile,
+		browser: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+		port:3000,
+		notify:true
+	})
+}
+//--------------------------------------------------
+//ОЧИСТКА
+//--------------------------------------------------
+function Clean(params) {
+	return del(path.clean)
+}
+//--------------------------------------------------
+//ОБРАБОТЧИКИ
+//--------------------------------------------------
+function HTML() {
+	return src(path.src.html)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.html))
+		.pipe(browsersync.stream())
+}
+function CSS_Dev() {
+	return src(path.src.css)
+		.pipe(sourcemaps.init())
+			.pipe(
+				scss({
+					outputStyle: "expanded"
+				})
+			)
+			// .pipe(
+			// 	gcmq()
+			// )
+			// .pipe(
+			// 	autoprefixer({
+			// 		overrideBrowserslist: ["last 5 version"],
+			// 		cascade:true
+			// 	})
+			// )
+		.pipe(sourcemaps.write())
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream())
+}
+function CSS() {
+	return src(path.src.css)
+		.pipe(
+			scss({
+				outputStyle: "expanded"
+			})
+		)
+		.pipe(
+			gcmq()
+		)
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ["last 5 version"],
+				cascade:true
+			})
+		)
+		.pipe(dest(path.build.css))
+		.pipe(clean_css())
+		.pipe(
+			rename({
+				extname: ".min.css"
+			})
+		)
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream())
+}
+function JS() {
+	return src(path.src.js)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.js))
+		.pipe(uglify())
+		.pipe(
+			rename({
+				extname: ".min.js"
+			})
+		)
+		.pipe(dest(path.build.js))
+		.pipe(browsersync.stream())
+}
+function IMAGES() {
+	return src(path.src.img)
+		.pipe(
+			imagemin({
+				interlaced: true,
+				progressive: true,
+				optimizationLevel: 3,//0 to7
+				svgoPlugins: [{removeViewBox: false}]//true
+			})
+		)
+		.pipe(dest(path.build.img))
+		.pipe(browsersync.stream())
+}
+//---ШРИФТЫ
+function FONTS(params) {
+	src(path.src.fonts)
+		.pipe(ttf2woff())
+		.pipe(dest(path.build.fonts))
+	return src(path.src.fonts)
+		.pipe(ttf2woff2())
+		.pipe(dest(path.build.fonts))
+}
+function FontsStyle(params) {
+	let file_content=fs.readFileSync(source_folder + '/scss/_fonts.scss');
+	if(file_content==''){
+		fs.writeFile(source_folder + '/scss/_fonts.scss', '', cb);
+		return fs.readdir(path.build.fonts, function(err,items){
+			if(items){
+				let c_fontname;
+				for(var i = 0; i < items.length; i++){
+					let fontname = items[i].split('.');
+					fontname = fontname[0];
+					if(c_fontname!=fontname){
+						fs.appendFile(source_folder + '/scss/_fonts.scss',
+						'@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb)
+					}
+					c_fontname = fontname
+				}
+			}
+		})
+	}
+}
+function cb() {}
+
+//--------------------------------------------------
+//ВОТЧЕР
+//--------------------------------------------------
+function WatchFiles(params) {
+	gulp.watch([path.watch.html],HTML)
+//	gulp.watch([path.watch.css],CSS)
+	gulp.watch([path.watch.css],CSS_Dev)
+	gulp.watch([path.watch.js],JS)
+	gulp.watch([path.watch.img],IMAGES)
+}
+//--------------------------------------------------
+//ТАСКИ
+//--------------------------------------------------
+//gulp.task('fonts',FontsStyle())//gulp fonts
+let dev=gulp.series(Clean,gulp.parallel(CSS_Dev,HTML,JS,IMAGES,FONTS),FontsStyle)
+let build=gulp.series(Clean,gulp.parallel(CSS_Dev,HTML,JS,IMAGES,FONTS),FontsStyle)
+//let build=gulp.series(Clean,gulp.parallel(CSS_Dev,HTML,JS,IMAGES,FONTS),FontsStyle)
+let watch=gulp.parallel(dev,WatchFiles,BrowserSync)
+let watch2=gulp.parallel(build,WatchFiles,BrowserSync)
+//--------------------------------------------------
+//EXPORT
+//--------------------------------------------------
+
+// exports.html=HTML
+// exports.css=CSS
+// exports.css=CSS_Dev
+// exports.js=JS
+// exports.images=IMAGES
+// exports.fonts=FONTS
+// exports.FontsStyle=FontsStyle
+exports.dev=dev
+exports.build=build
+exports.watch=watch
+exports.watch2=watch2
+exports.default=watch
