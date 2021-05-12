cee75b56f970149456de0e1312d8abf9a9385c4b
DevCoderGod <devcodergod@gmail.com>
2021-05-13 01:42:55 +0300

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,11 @@
-//--------------------------------------------------
-// function defaultTask(cb) {
-// 	// place code for your default task here
-// 	cb();
-//  }
- 
-//  exports.default = defaultTask
-//--------------------------------------------------
-//ПЕРЕМЕННЫЕ ПАПОК
-//--------------------------------------------------
-//let project_folder = "dist"
 let project_folder = require("path").basename(__dirname)
 let source_folder = "#src"
 let fs = require("fs")
-// let indexFile = "index_dev.html"
-let indexFile = "index.html"
-
-//--------------------------------------------------
+let mode = 'dev'
+function cb() {}
+//-----------------------------------------------
 //ПУТИ
-//--------------------------------------------------
+//-----------------------------------------------
 let path ={
 	build:{
 		html: project_folder+"/",
@@ -41,9 +29,9 @@ let path ={
 	},
 	clean: "./"+project_folder+"/"
 }
-//--------------------------------------------------
+//-----------------------------------------------
 //ПЕРЕМЕННЫЕ
-//--------------------------------------------------
+//-----------------------------------------------
 let {src,dest}=require("gulp")
 let gulp=require('gulp')
 
@@ -60,57 +48,72 @@ let imagemin = require('gulp-imagemin');
 let ttf2woff = require('gulp-ttf2woff');
 let ttf2woff2 = require('gulp-ttf2woff2');
 let sourcemaps = require('gulp-sourcemaps');
-//--------------------------------------------------
-//browser-sync
-//--------------------------------------------------
-function BrowserSync(params) {
-	browsersync.init({
-		server:{
-			baseDir:"./"+project_folder+"/"
-		},
-		index: indexFile,
-		browser: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-		port:3000,
-		notify:true
-	})
+
+//-----------------------------------------------
+//сброс режима разработки
+//-----------------------------------------------
+function production(cb){
+	mode=''
+	cb()
 }
-//--------------------------------------------------
+//-----------------------------------------------
+//browser-sync
+//-----------------------------------------------
+function BrowserSync() {
+	switch (mode) {
+		case "dev":
+			browsersync.init({
+				server:{
+					baseDir:"./"+project_folder+"/"
+				},
+				index: "index_dev.html",
+				browser: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+				port:3000,
+				notify:true
+			})
+			break;
+		default:
+			browsersync.init({
+				server:{
+					baseDir:"./"+project_folder+"/"
+				},
+				index: "index.html",
+				browser: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+				port:3000,
+				notify:true
+			})
+			break;
+	}
+}
+//-----------------------------------------------
 //ОЧИСТКА
-//--------------------------------------------------
-function Clean(params) {
+//-----------------------------------------------
+function Clean() {
 	return del(path.clean)
 }
-//--------------------------------------------------
+//-----------------------------------------------
 //ОБРАБОТЧИКИ
-//--------------------------------------------------
-function HTML() {
+//-----------------------------------------------
+function html() {
 	return src(path.src.html)
 		.pipe(fileinclude())
 		.pipe(dest(path.build.html))
 		.pipe(browsersync.stream())
 }
-function CSS_Dev() {
+
+function cssDev() {
 	return src(path.src.css)
 		.pipe(sourcemaps.init())
-			.pipe(
-				scss({
-					outputStyle: "expanded"
-				})
-			)
-			// .pipe(
-			// 	gcmq()
-			// )
-			// .pipe(
-			// 	autoprefixer({
-			// 		overrideBrowserslist: ["last 5 version"],
-			// 		cascade:true
-			// 	})
-			// )
+		.pipe(
+			scss({
+				outputStyle: "expanded"
+			})
+		)
 		.pipe(sourcemaps.write())
 		.pipe(dest(path.build.css))
 		.pipe(browsersync.stream())
 }
-function CSS() {
+function css() {
 	return src(path.src.css)
 		.pipe(
 			scss({
@@ -136,7 +139,15 @@ function CSS() {
 		.pipe(dest(path.build.css))
 		.pipe(browsersync.stream())
 }
-function JS() {
+
+function jsDev() {
+	return src(path.src.js)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.js))
+		.pipe(browsersync.stream())
+}
+
+function js() {
 	return src(path.src.js)
 		.pipe(fileinclude())
 		.pipe(dest(path.build.js))
@@ -149,6 +160,7 @@ function JS() {
 		.pipe(dest(path.build.js))
 		.pipe(browsersync.stream())
 }
+
 function IMAGES() {
 	return src(path.src.img)
 		.pipe(
@@ -162,8 +174,10 @@ function IMAGES() {
 		.pipe(dest(path.build.img))
 		.pipe(browsersync.stream())
 }
+//-----------------------------------------------
 //---ШРИФТЫ
-function FONTS(params) {
+//-----------------------------------------------
+function fonts() {
 	src(path.src.fonts)
 		.pipe(ttf2woff())
 		.pipe(dest(path.build.fonts))
@@ -171,11 +185,12 @@ function FONTS(params) {
 		.pipe(ttf2woff2())
 		.pipe(dest(path.build.fonts))
 }
-function FontsStyle(params) {
+
+function fontsStyle(cb) {
 	let file_content=fs.readFileSync(source_folder + '/scss/_fonts.scss');
 	if(file_content==''){
-		fs.writeFile(source_folder + '/scss/_fonts.scss', '', cb);
-		return fs.readdir(path.build.fonts, function(err,items){
+		fs.writeFileSync(source_folder + '/scss/_fonts.scss','');
+		fs.readdir(path.build.fonts,function(err,items){
 			if(items){
 				let c_fontname;
 				for(var i = 0; i < items.length; i++){
@@ -190,45 +205,43 @@ function FontsStyle(params) {
 			}
 		})
 	}
+	cb()
 }
-function cb() {}
-
-//--------------------------------------------------
+//-----------------------------------------------
+//---удалить файл index_dev.html
+//-----------------------------------------------
+function delIndexDev() {
+	return del(path.build.html+"index_dev.html")
+}
+//-----------------------------------------------
 //ВОТЧЕР
-//--------------------------------------------------
-function WatchFiles(params) {
-	gulp.watch([path.watch.html],HTML)
-	gulp.watch([path.watch.css],CSS)
-	// gulp.watch([path.watch.css],CSS_Dev)
-	gulp.watch([path.watch.js],JS)
-	gulp.watch([path.watch.img],IMAGES)
+//-----------------------------------------------
+function WatchFiles() {
+	switch (mode) {
+		case "dev":
+			gulp.watch([path.watch.html],html)
+			gulp.watch([path.watch.css],cssDev)
+			gulp.watch([path.watch.js],jsDev)
+			gulp.watch([path.watch.img],IMAGES)
+			break;
+		default:
+			gulp.watch([path.watch.html],html)
+			gulp.watch([path.watch.css],css)
+			gulp.watch([path.watch.js],js)
+			gulp.watch([path.watch.img],IMAGES)
+			break;
+	}
 }
-//--------------------------------------------------
+//-----------------------------------------------
 //ТАСКИ
-//--------------------------------------------------
-//gulp.task('fonts',FontsStyle())//gulp fonts
-let dev=gulp.series(Clean,gulp.parallel(CSS,HTML,JS,IMAGES,FONTS),FontsStyle)
-let build=gulp.series(Clean,gulp.parallel(CSS,HTML,JS,IMAGES,FONTS),FontsStyle)
+//-----------------------------------------------
+let buildDev=gulp.series(Clean,fonts,fontsStyle,gulp.parallel(html,cssDev,jsDev,IMAGES))
+let dev=gulp.series(buildDev,gulp.parallel(WatchFiles,BrowserSync))
 
-// let dev=gulp.series(Clean,gulp.parallel(CSS_Dev,HTML,JS,IMAGES,FONTS),FontsStyle)
-// let build=gulp.series(Clean,gulp.parallel(CSS_Dev,HTML,JS,IMAGES,FONTS),FontsStyle)
-
-
-let watch=gulp.parallel(dev,WatchFiles,BrowserSync)
-let watch2=gulp.parallel(build,WatchFiles,BrowserSync)
-//--------------------------------------------------
+let buildProd=gulp.series(production,Clean,fonts,fontsStyle,gulp.parallel(html,css,js,IMAGES),delIndexDev)
+let prod=gulp.series(buildProd,gulp.parallel(WatchFiles,BrowserSync))
+//-----------------------------------------------
 //EXPORT
-//--------------------------------------------------
-
-// exports.html=HTML
-// exports.css=CSS
-// exports.css=CSS_Dev
-// exports.js=JS
-// exports.images=IMAGES
-// exports.fonts=FONTS
-// exports.FontsStyle=FontsStyle
-exports.dev=dev
-exports.build=build
-exports.watch=watch
-exports.watch2=watch2
-exports.default=watch
+//-----------------------------------------------
+exports.prod=prod
+exports.default=dev
